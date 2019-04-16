6708257b47ffed79c5699f89c5ac32d48d3d4e28
Andrew Price <andrew@lemoncog.com>
2019-04-16 16:37:26 +0100

    Added mutation update to ensure data from last calld updateQuestionPage gql is in local store

--- eq-author/src/App/page/Design/QuestionPageEditor/withUpdateQuestionPage.js
+++ eq-author/src/App/page/Design/QuestionPageEditor/withUpdateQuestionPage.js
@@ -3,11 +3,18 @@ import updatePageMutation from "graphql/updatePage.graphql";
 import pageFragment from "graphql/fragments/page.graphql";
 import { filter } from "graphql-anywhere";
 
+let updateQuestionCount = 0;
+
 export const mapMutateToProps = ({ mutate }) => ({
   onUpdateQuestionPage: page => {
     const data = filter(pageFragment, page);
+
+    const currentUpdateQuestionCount = updateQuestionCount++;
+
     return mutate({
-      variables: { input: data },
+      variables: { 
+        input: data
+      },
       optimisticResponse: {
         updateQuestionPage: {
           ...page,
@@ -15,6 +22,23 @@ export const mapMutateToProps = ({ mutate }) => ({
           __typename: "QuestionPage",
         },
       },
+      update: (proxy, { data: {updateQuestionPage}}) => {
+        // Update the local cache with the last called mutation to avoid race condition caused by
+        // second mutation returning before the previous
+        if (currentUpdateQuestionCount === updateQuestionCount - 1) {
+          console.log({currentUpdateQuestionCount, updateQuestionCount, updateQuestionPage}, {
+            id: `${updateQuestionPage.__typename}${updateQuestionPage.id}`,
+            fragment: pageFragment,
+            data: updateQuestionPage
+          });
+          proxy.writeFragment( {
+            id: `${updateQuestionPage.__typename}${updateQuestionPage.id}`,
+            fragment: pageFragment,
+            data: updateQuestionPage
+          });     
+
+        }
+      }
     });
   },
 });
