63fd967462d2b572116dd53c16476a33b3206e70
thomas.mcauliffe <tjamesmac@gmail.com>
2020-05-19 13:26:25 -0700

    Add structure

--- eq-author/src/App/sharing/SharePage.js
+++ eq-author/src/App/sharing/SharePage.js
@@ -5,6 +5,9 @@ import Header from "components/EditorLayout/Header";
 import ScrollPane from "components/ScrollPane";
 import { Grid } from "components/Grid";
 import MainCanvas from "components/MainCanvas";
+import Panel from "components/Panel";
+
+import { colors } from "constants/theme";
 
 const Container = styled.div`
   display: flex;
@@ -23,6 +26,36 @@ const StyledMainCanvas = styled(MainCanvas)`
   max-width: 80em;
 `;
 
+const ShareLayout = styled.div`
+  padding: 2em;
+`;
+
+const PageTitle = styled.h2`
+  font-size: 1.1em;
+  font-weight: bold;
+  color: ${colors.text};
+  margin: 0 0 1em;
+`;
+const SectionTitle = styled.h3`
+  font-size: 1.1em;
+  font-weight: bold;
+  color: ${colors.text};
+  margin: 0 0 1em;
+`;
+const PageDescription = styled.p`
+  margin: 0.1em 0 1em;
+  color: ${colors.textLight};
+`;
+
+const PageSection = styled.div`
+  &:not(:last-of-type) {
+    border-bottom: 1px solid #e0e0e0;
+  }
+  &:first-of-type {
+    border-top: 1px solid #e0e0e0;
+  }
+`;
+
 const SharePage = ({ match }) => {
   return (
     <Container>
@@ -30,7 +63,29 @@ const SharePage = ({ match }) => {
       <StyledGrid>
         <ScrollPane permanentScrollBar data-test="sharing-page-content">
           <StyledMainCanvas>
-            <div>HELLO WORLD AM I BETTER NOW?</div>
+            <Panel>
+              <ShareLayout>
+                {/* Page title */}
+                <PageTitle>Share your questionnaire</PageTitle>
+                {/* Page description */}
+                <PageDescription>
+                  You can share your questionnaire with anyone who has an
+                  account with Author.
+                </PageDescription>
+                {/* Section One */}
+                <PageSection>
+                  <SectionTitle>Public access</SectionTitle>
+                  {/* Public access */}
+                </PageSection>
+                {/* Section Two */}
+                <PageSection>
+                  <SectionTitle>Editors</SectionTitle>
+                  <SectionTitle>Add Editors</SectionTitle>
+                  {/* Editors */}
+                  {/* Add Editors */}
+                </PageSection>
+              </ShareLayout>
+            </Panel>
           </StyledMainCanvas>
         </ScrollPane>
       </StyledGrid>
