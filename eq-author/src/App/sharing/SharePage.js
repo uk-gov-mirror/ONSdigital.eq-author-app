9bfd1e70d2fb36d9d1f4cd7a4387a233d02fe5d4
thomas.mcauliffe <tjamesmac@gmail.com>
2020-05-19 14:33:31 -0700

    Add colours and boiler plate

--- eq-author/src/App/sharing/SharePage.js
+++ eq-author/src/App/sharing/SharePage.js
@@ -7,8 +7,12 @@ import { Grid } from "components/Grid";
 import MainCanvas from "components/MainCanvas";
 import Panel from "components/Panel";
 
-import { colors } from "constants/theme";
+import { Field } from "components/Forms";
+import { UncontrolledInput } from "components/Forms/Input";
 
+import { colors } from "constants/theme";
+// Boiler plate structure
+// ------------------------------------------------
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -25,7 +29,12 @@ const StyledMainCanvas = styled(MainCanvas)`
   padding: 0 0.5em 0 1em;
   max-width: 80em;
 `;
-
+// ------------------------------------------------
+//
+// Basic styling
+// Tidy up once everything is sorted
+// Want to isolate components that could be reusable
+// ------------------------------------------------
 const ShareLayout = styled.div`
   padding: 2em;
 `;
@@ -36,26 +45,47 @@ const PageTitle = styled.h2`
   color: ${colors.text};
   margin: 0 0 1em;
 `;
+
 const SectionTitle = styled.h3`
   font-size: 1.1em;
   font-weight: bold;
   color: ${colors.text};
   margin: 0 0 1em;
 `;
+
 const PageDescription = styled.p`
   margin: 0.1em 0 1em;
   color: ${colors.textLight};
 `;
 
 const PageSection = styled.div`
-  &:not(:last-of-type) {
-    border-bottom: 1px solid #e0e0e0;
-  }
+  padding: 2em 0;
   &:first-of-type {
     border-top: 1px solid #e0e0e0;
+    border-bottom: 1px solid #e0e0e0;
   }
 `;
 
+const SearchInput = styled(UncontrolledInput)`
+  padding: 0.5em;
+  font-size: 1em;
+  width: 100%;
+  border: 1px solid ${colors.borders};
+`;
+
+const InsetText = styled.div`
+  border-left: 10px solid #033e58;
+  background: #eff0f9;
+  padding: 1em;
+`;
+
+const SearchContainer = styled.div`
+  margin: 0 0 2em;
+  background: #ebeef0;
+  height: 100%;
+  padding: 1em 1em 0.3em;
+`;
+// ------------------------------------------------
 const SharePage = ({ match }) => {
   return (
     <Container>
@@ -65,24 +95,41 @@ const SharePage = ({ match }) => {
           <StyledMainCanvas>
             <Panel>
               <ShareLayout>
-                {/* Page title */}
                 <PageTitle>Share your questionnaire</PageTitle>
-                {/* Page description */}
                 <PageDescription>
                   You can share your questionnaire with anyone who has an
                   account with Author.
+                  {/* Need to add link here */}
+                  <div>get shareable link goes here</div>
                 </PageDescription>
-                {/* Section One */}
                 <PageSection>
                   <SectionTitle>Public access</SectionTitle>
+
+                  {/* Banner goes here */}
+                  <InsetText>
+                    Let anyone with an Author account view your questionnaire.
+                    If public access is off, only editors
+                  </InsetText>
+
+                  {/* Banner goes here */}
                   {/* Public access */}
                 </PageSection>
-                {/* Section Two */}
                 <PageSection>
                   <SectionTitle>Editors</SectionTitle>
+                  <InsetText>
+                    Editors can edit questionnaire content, add comments, delete
+                    the questionnaire and add other editors.
+                  </InsetText>
+                </PageSection>
+                <PageSection>
                   <SectionTitle>Add Editors</SectionTitle>
-                  {/* Editors */}
-                  {/* Add Editors */}
+                  Search for someone using their name or email address.
+                  <SearchContainer>
+                    <Field>
+                      <SearchInput placeholder="Tom is cool" />
+                      {/* Add button here */}
+                    </Field>
+                  </SearchContainer>
                 </PageSection>
               </ShareLayout>
             </Panel>
