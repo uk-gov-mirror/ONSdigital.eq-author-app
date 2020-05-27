97ed360685ab9699d3bd1bb92b9f5e4261d5815b
thomas.mcauliffe <tjamesmac@gmail.com>
2020-05-27 10:48:07 -0700

    Tidy up and decide on method for query

--- eq-author/src/App/sharing/ShareContent/index.js
+++ eq-author/src/App/sharing/ShareContent/index.js
@@ -5,11 +5,15 @@ import CustomPropTypes from "custom-prop-types";
 import { Query, useMutation } from "react-apollo";
 import GET_QUESTIONNAIRE from "../graphql/GetQuestionnaire.graphql";
 import TOGGLE_PUBLIC_MUTATION from "../graphql/TogglePublicMutation.graphql";
+import ALL_USERS from "../graphql/AllUsers.graphql";
 import config from "config";
 
 import Loading from "components/Loading";
 import Error from "components/Error";
 import ToggleSwitch from "components/buttons/ToggleSwitch";
+import InformationPanel from "components/Panel";
+
+import { useQuery } from "@apollo/react-hooks";
 
 import { withShowToast } from "components/Toasts";
 import {
@@ -20,7 +24,6 @@ import {
   SectionTitle,
   EditorTitle,
   SearchInput,
-  InsetText,
   SearchContainer,
   Separator,
   Wrapper,
@@ -29,6 +32,19 @@ import {
   AddButton,
   Described,
 } from "../styles";
+
+// ------------------------------------------------
+/*
+I want to be able to wrap the sharing page with the three necessary extras
+
+Succeeded in creating a double wrapped query with HOC's
+problem is I think it's redundant
+
+ON WEDNESDAY
+Going to rewrite to use the useQuery hook and batch it
+
+*/
+// ------------------------------------------------
 
 const propType = {
   TogglePublicLabel: {
@@ -112,17 +128,17 @@ const Sharing = ({ questionnaire, toast }) => {
             <TogglePublicLabel text="On" isActive={pub} />
           </Separator>
         </Wrapper>
-        <InsetText>
+        <InformationPanel>
           Let anyone with an Author account view your questionnaire. If public
           access is off, only editors
-        </InsetText>
+        </InformationPanel>
       </Section>
       <Section>
         <SectionTitle>Editors</SectionTitle>
-        <InsetText>
+        <InformationPanel>
           Editors can edit questionnaire content, add comments, delete the
           questionnaire and add other editors.
-        </InsetText>
+        </InformationPanel>
       </Section>
       <Section>
         <EditorTitle>Add Editors</EditorTitle>
@@ -145,23 +161,9 @@ const Sharing = ({ questionnaire, toast }) => {
 };
 
 // ------------------------------------------------
-const UnwrappedSharing = ({ loading, error, data, showToast }) => {
-  if (loading) {
-    return <Loading height="38rem">Page loading…</Loading>;
-  }
-  if (error) {
-    return <Error>Oops! Something went wrong</Error>;
-  }
-  const { questionnaire } = data;
-  // Reduces prop load by not spreading the props
-  // {...props} <--- like that
-  return <Sharing questionnaire={questionnaire} toast={showToast} />;
-};
 
-const ToastedUnwrappedSharing = flowRight(withShowToast)(UnwrappedSharing);
-
-const ShareContent = props => {
-  return (
+const OneWrapper = Component => {
+  const WrapperOne = props => (
     <Query
       query={GET_QUESTIONNAIRE}
       variables={{
@@ -172,14 +174,25 @@ const ShareContent = props => {
       }}
       fetchPolicy="no-cache"
     >
-      {innerprops => <ToastedUnwrappedSharing {...innerprops} />}
+      {innerprops => {
+        if (innerprops.loading) {
+          return <Loading height="38rem">Page loading…</Loading>;
+        }
+        if (innerprops.error) {
+          return <Error>Oops! Something went wrong</Error>;
+        }
+        return <Component questionnaire={innerprops.data} {...innerprops} />;
+      }}
     </Query>
   );
+  return WrapperOne;
 };
+
+const ToastedUnwrappedSharing = flowRight(withShowToast, OneWrapper)(Sharing);
 
 TogglePublicLabel.propTypes = propType.ToggleLabelComp;
 Sharing.propTypes = propType.Share;
-UnwrappedSharing.propTypes = propType.UnwrappedSharing;
-ShareContent.propTypes = propType.ShareContent;
+// UnwrappedSharing.propTypes = propType.UnwrappedSharing;
+// ShareContent.propTypes = propType.ShareContent;
 
-export default ShareContent;
+export default ToastedUnwrappedSharing;
