acbfab34be2aea691076e2708938c6e53890731b
thomas.mcauliffe <tjamesmac@gmail.com>
2020-05-29 11:37:21 -0700

    Adjust isOwner styling

--- eq-author/src/App/sharing/styles/UserList.js
+++ eq-author/src/App/sharing/styles/UserList.js
@@ -1,61 +1,15 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { colors } from "constants/theme";
 
 import iconClose from "./icons/icon-close.svg";
 
 import { Separator } from "./index";
 
-// "invert(100%) sepia(100%) saturate(0%) hue-rotate(288deg) brightness(102%) contrast(102%)",
 const iconColors = {
   owner: "none",
   notOwner:
     "invert(46%) sepia(4%) saturate(4439%) hue-rotate(158deg) brightness(94%) contrast(103%)",
 };
-
-export const List = styled.ul`
-  padding: 0;
-  margin: 0;
-  list-style: none;
-  color: ${colors.text};
-  max-height: 10em;
-`;
-
-export const ListItem = styled.li`
-  margin: 0 0 0.4em;
-  display: flex;
-  align-items: center;
-  position: relative;
-  color: #666;
-  div {
-    background-color: ${props =>
-      props.isOwner ? colors.black : colors.lightMediumGrey};
-    color: ${props => (props.isOwner ? colors.white : colors.text)};
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0.3em 0.5em;
-    /* background-color: ${colors.lightGrey}; */
-  }
-`;
-
-export const UserName = styled.div`
-  color: ${colors.text};
-  font-weight: bold;
-  font-size: 0.9em;
-`;
-
-export const UserEmail = styled.div`
-  color: ${colors.textLight};
-  font-size: 0.9em;
-  margin-left: 0.9em;
-  opacity: 0.7;
-`;
-
-export const UserIcon = styled.img`
-  margin-right: 0.25em;
-  height: 1.1em;
-  filter: ${props => (props.isOwner ? iconColors.owner : iconColors.notOwner)};
-`;
 
 export const RemoveButton = styled.button`
   appearance: none;
@@ -75,6 +29,41 @@ export const RemoveButton = styled.button`
   }
 `;
 
+export const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  color: ${colors.text};
+`;
+
+export const ListItem = styled.li`
+  margin: 0 0 0.4em;
+  display: flex;
+  align-items: center;
+  position: relative;
+  color: #666;
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0.3em 0.5em;
+  }
+`;
+
+export const UserName = styled.span`
+  color: ${colors.text};
+  font-weight: bold;
+  font-size: 0.9em;
+`;
+
+export const UserEmail = styled.div`
+  color: ${colors.textLight};
+  font-size: 0.9em;
+  margin-left: 0.9em;
+  opacity: 0.7;
+  font-weight: bold;
+`;
+
 export const UserOwner = styled.span`
   margin-left: 0.9em;
   margin-right: 1.2em;
@@ -83,8 +72,35 @@ export const UserOwner = styled.span`
   opacity: 0.7;
 `;
 
+export const UserIcon = styled.img`
+  margin-right: 0.25em;
+  height: 1.1em;
+  filter: ${iconColors.notOwner};
+`;
+
 export const Line = styled(Separator)`
   margin-left: 0.6em;
-  border-left: 1px solid
-    ${props => (props.isOwner ? colors.white : colors.blue)};
+  border-left: 1px solid ${colors.blue};
+`;
+
+export const UserContainer = styled.div`
+  background-color: ${props =>
+    props.isOwner ? colors.black : colors.lightMediumGrey};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0.3em 0.5em;
+  ${props =>
+    props.isOwner &&
+    css`
+      ${UserName} {
+        color: ${colors.white};
+      }
+      ${UserIcon} {
+        filter: ${iconColors.owner};
+      }
+      ${Line} {
+        border-left: 1px solid ${colors.white};
+      }
+    `};
 `;
