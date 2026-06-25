570f17cec36a58d02c15ebbd374509571022aa9c
oms09 <omar.moulana@ons.gov.uk>
2026-06-25 14:32:37 +0100

    new cir post endpoint

--- eq-author-api/schema/resolvers/utils/publishSchema.js
+++ eq-author-api/schema/resolvers/utils/publishSchema.js
@@ -77,7 +77,7 @@ const postSchema = async (
   publishResult
 ) => {
   await authorisedRequest(
-    `${gateway}publish_collection_instrument?${query}`,
+    `${gateway}collection_instrument?${query}`,
     audience,
     {
       method: "POST",
